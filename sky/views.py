c104a0c26d00325a10aa769250a6a15257b40d7c
Hira Khan <phr.hirakhan@gmail.com>
2022-11-02 21:48:52 +0100

    made changes

--- sky/views.py
+++ sky/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello! That's me.")
+    return HttpResponse("This response is from sky <a href='/'>Home</a>")
